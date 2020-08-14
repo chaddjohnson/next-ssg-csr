35590c7057d1bc8f533ad887ed1aae6c8ab46f2e
Chad Johnson <chad.d.johnson@gmail.com>
2020-08-13 22:17:35 -0400

    Catch-all test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,5 @@
-import { StaticRouter, BrowserRouter, Switch, Route } from 'react-router-dom';
-import ProductPage from './products/[slug]';
-
 function MyApp({ Component, pageProps }) {
-  const Router = typeof window === 'undefined' ? StaticRouter : BrowserRouter;
-
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/products/:slug">
-          <ProductPage {...pageProps} />
-        </Route>
-        <Route>
-          <Component {...pageProps} />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
